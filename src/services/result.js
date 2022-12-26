7311b11452e92105dafe2f7ac44fb1b6d102ea86
AlexandreUff <alexandre_magno@id.uff.br>
2022-12-26 17:27:07 -0300

    change TBM function for caloricExpenditure and set some adjusts

--- src/services/result.js
+++ src/services/result.js
@@ -1,17 +1,56 @@
 import React from "react";
 
-const BMR = (data)=>{
-    return data.sexo === "masc" ? 66+(13.7*data.peso)+(5*data.estatura)-(6.8*data.idade) : 655+(9.6*data.peso)+(1.8*data.estatura)-(4.7*data.idade);
+const getCaloricExpenditure = (data)=>{
+    const male = 66+(13.7*data.peso)+(5*data.estatura)-(6.8*data.idade);
+    const female = 655+(9.6*data.peso)+(1.8*data.estatura)-(4.7*data.idade);
+
+    let caloricExpenditure;
+    let metabolicRate = data.sexo === "masc" ? male : female;
+
+    switch(data.atividade){
+        case "nad":
+            caloricExpenditure = metabolicRate * 1.2
+            break;
+        case "lev":
+            caloricExpenditure = metabolicRate * 1.375
+            break;
+        case "mod":
+            caloricExpenditure = metabolicRate * 1.55
+            break;
+        case "alt":
+            caloricExpenditure = metabolicRate * 1.725
+            break;
+        case "ext":
+            caloricExpenditure = metabolicRate * 1.9
+            break;
+    }
+
+    return {
+        metabolicRate,
+        caloricExpenditure,
+    }
 }
 
 const Hipertrophy = (data) => {
     console.log("Hipertrophy",data);
-    console.log("TMB",BMR(data));
+
+    const { metabolicRate, caloricExpenditure } = getCaloricExpenditure(data);
+
+    const proteins = {
+        min: data.peso * 1.6,
+        max: data.peso * 2.2,
+    };
+    const fat = data.peso;
+
+    const carbo = data.peso * 4;
+
+    console.log("TMB",metabolicRate);
+    console.log("GASTO",caloricExpenditure);
 }
 
 const Slimming = (data) => {
     console.log("Emagrecimento",data);
-    console.log("TMB",BMR(data));
+    console.log("TMB",getCaloricExpenditure(data));
 }
 
 export default function Result(peso,estatura, idade, atividade, sexo, objetivo){
@@ -24,7 +63,7 @@ export default function Result(peso,estatura, idade, atividade, sexo, objetivo){
         console.log("objetivo 0",objetivo[0].checked);
         console.log("objetivo 1",objetivo[1].checked);
 
-        const data = {
+        const userData = {
             peso: +peso.value,
             estatura: +estatura.value,
             idade: +idade.value,
@@ -33,8 +72,8 @@ export default function Result(peso,estatura, idade, atividade, sexo, objetivo){
         }
 
         if(objetivo[0].checked){
-            Hipertrophy(data);
+            Hipertrophy(userData);
         } else if(objetivo[1].checked){
-            Slimming(data);
+            Slimming(userData);
         }
 }
